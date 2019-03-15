35671091000f7c9b6d701ebd81bc0f4da41ae6d0
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2019-03-15 13:44:33 -0400

    go version now as functional as lua

--- client.go
+++ client.go
@@ -1,8 +1,12 @@
 package main
 
+// source:  https://github.com/etcd-io/etcd/tree/master/client
+
 import (
 	"context"
+	"fmt"
 	"log"
+	"os/exec"
 	"time"
 
 	"go.etcd.io/etcd/client"
@@ -21,26 +25,30 @@ func main() {
 		log.Fatal(err)
 	}
 	kapi := client.NewKeysAPI(c)
-	// // set "/foo" key with "bar" value
-	// log.Print("Setting '/foo' key with 'bar' value")
-	resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// } else {
-	// 	// print common key info
-	// 	log.Printf("Set is done. Metadata is %q\n", resp)
-	// }
-	// get "/foo" key's value
 
 	log.Print("Getting '/foo' key value")
-	path := "path/to/key"
-	resp, err = kapi.Get(context.Background(), path, nil)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		// print common key info
-		log.Printf("Get is done. Metadata is %q\n", resp)
-		// print value
-		log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+	path := "/path/to/key"
+	for true {
+		resp, err := kapi.Watcher(path, nil).Next(context.Background())
+
+		// log.Printf("Result: %q\n", resp.Action)
+
+		if err != nil {
+			log.Fatal(err)
+		} else if resp.Action == "set" {
+			cmd, err := exec.Command("/bin/bash", "/usr/sbin/sc_lock").Output()
+			// cmdOut, err := cmd.Output()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(cmd))
+		} else if resp.Action == "expire" {
+			cmd, err := exec.Command("/bin/bash", "/usr/sbin/sc_unlock").Output()
+			// cmdOut, err := cmd.Output()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(cmd))
+		}
 	}
 }
